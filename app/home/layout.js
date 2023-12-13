32e9c4800e92409099117b8bf1eeaf403c543d95
vugvr617 <vnasraddinli@gmail.com>
2023-12-13 23:36:36 +0100

    Update video source

--- app/home/layout.js
+++ app/home/layout.js
@@ -34,7 +34,7 @@ const HomeLayout = ({ children }) => {
             loop
             className="w-full h-full -z-[1] fixed object-cover"
             src={
-              "https://s3-figma-videos-production-sig.figma.com/video/962310623581735403/TEAM/d2cf/30d5/-a54b-401f-a464-27599ffabb4c?Expires=1702252800&Signature=jVMI6-M-343SKDPMUPqz4k-1SxSdMMvcBXbS6AnALJeHxku6k77U7sKOnnPC7JI51GSEEgSj7x52PomB7HGdcEJyaE6v3b5wapYfFbBvAVetWUJb8Qa1sTarExMqT6PLjHfyVU9Rtbn5Wuc1pCrn2zpvnrI~IyxmpgmGYxr5t1Pg1KAU~SuP0LFPVAUp2TbrT0EPEuCk5QQ2V29okwAUsxFRzoC9U6Mv0f9xFnDY21DJ5J1gxy1C~J4jiK32D6dvdBLy2SCHOSz4ZkjqYgboSx4ITP7-Oox7dXgNUNUS9qxl5kvE~0X-Ny3GbrLONCYiXgWIbrhqP47~R5fRtKDhVw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+              "https://s3-figma-videos-production-sig.figma.com/video/962310623581735403/TEAM/d2cf/30d5/-a54b-401f-a464-27599ffabb4c?Expires=1703462400&Signature=YL2xMd7pLPStC5mXl52g87gTa16DpmMqC6mFxu687rk9GRUHZKFEO9tSl~NUZXZQiA~gJTZd~hpiCDOPI9lqLjK5s~2qbA1-TD4c6IFnx821resuBks-wjwii6TbNtpQz1OTY2E1TDvY6XQZfF9QBYCt1WMV-Ic6j6nUil9xE9T2RhcVSCIuZbVoP8Vd3QAgBciClk1AQLn4YF~rjoHsM1Dj5F-G138Qxt4lhWhaHf1X2v7g0oVc4Z1xaQbVlD-GAiXs5KEZ3MhOlVtxezLEszoNexvJwqqj-Gzz1VnfvfIGPCvWdPP4TZ6letGE-qx9zJVWH-40Agdk4uLcwBjETg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
             }
           />
           {children}
